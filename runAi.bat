15a9611e730896e4f5e2255e5253e57393902ba1
RomagnaMarco <marco.romagna@yahoo.com>
2022-11-20 20:12:11 -0800

    added better way for hardcode

--- runAi.bat
+++ runAi.bat
@@ -1,3 +1,3 @@
-call C:\Users\VNMen\anaconda3\condabin\activate.bat python37
+call C:\Users\Marco\anaconda3\condabin\activate.bat py37
 python --version
-python C:\Users\VNMen\Documents\SrDesign\CSRNet-crowd_analysis\analyze.py C:\Users\VNMen\Documents\SrDesign\CSRNet-crowd_analysis\ckpts\model.pth.tar C:\Users\VNMen\Documents\SrDesign\CSRNet-crowd_analysis\Shanghai\part_A_final\test_data\images\IMG_2.jpg
+python C:\Users\Marco\CSRNet-crowd_analysis\analyze.py C:\Users\Marco\CSRNet-crowd_analysis\ckpts\model-76.28.pth.tar C:\Users\Marco\CSRNet-crowd_analysis\Shanghai\part_A_final\test_data\images\IMG_27.jpg
